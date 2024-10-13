2ca26a5965a7816a85da907523ece1717d5a22ea
QiangYu <yuqiang0304@gmail.com>
2024-10-13 15:01:28 +0800

    rollback

--- Chapter06/defs.h
+++ Chapter06/defs.h
@@ -158,7 +158,6 @@ void            uartputc_sync(int);
 int             uartgetc(void);
 
 // vm.c
-void            vmprint(pagetable_t pgtbl, int level);
 void            kvminit(void);
 void            kvminithart(void);
 uint64          kvmpa(uint64);
